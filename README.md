b64e28d84cc369e1b9bd037bee69482ff1fe7b71
efremOffice <143795390+efremOffice@users.noreply.github.com>
2025-12-07 22:58:51 +0300

    test

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Project info
 
-**URL**: https://lovable.dev/projects/1f2a01e4-c60a-48c0-8892-ebc6f38f5636
+**URL**: https://lovable.dev/projects/196b030e-1d53-4bd0-8295-63870fc4d2db
 
 ## How can I edit this code?
 
@@ -10,7 +10,7 @@ There are several ways of editing your application.
 
 **Use Lovable**
 
-Simply visit the [Lovable Project](https://lovable.dev/projects/1f2a01e4-c60a-48c0-8892-ebc6f38f5636) and start prompting.
+Simply visit the [Lovable Project](https://lovable.dev/projects/196b030e-1d53-4bd0-8295-63870fc4d2db) and start prompting.
 
 Changes made via Lovable will be committed automatically to this repo.
 
@@ -62,7 +62,7 @@ This project is built with:
 
 ## How can I deploy this project?
 
-Simply open [Lovable](https://lovable.dev/projects/1f2a01e4-c60a-48c0-8892-ebc6f38f5636) and click on Share -> Publish.
+Simply open [Lovable](https://lovable.dev/projects/196b030e-1d53-4bd0-8295-63870fc4d2db) and click on Share -> Publish.
 
 ## Can I connect a custom domain to my Lovable project?
 
@@ -70,4 +70,4 @@ Yes, you can!
 
 To connect a domain, navigate to Project > Settings > Domains and click Connect Domain.
 
-Read more here: [Setting up a custom domain](https://docs.lovable.dev/tips-tricks/custom-domain#step-by-step-guide)
+Read more here: [Setting up a custom domain](https://docs.lovable.dev/features/custom-domain#custom-domain)
--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,52 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "next-themes";
-
-import { MainLayout } from "@/components/layout/main-layout";
-import { LoginPage } from "@/pages/LoginPage";
-import { DashboardPage } from "@/pages/DashboardPage";
-import { StreamsPage } from "@/pages/StreamsPage";
-import { UnifiedAlertsPage } from "@/pages/UnifiedAlertsPage";
-import { UnifiedReportsPage } from "@/pages/UnifiedReportsPage";
-import { FlowDetailPage } from "@/pages/flows/flow-detail/FlowDetailPage";
-import { FlowEditorRoute } from '@/pages/flows/flow-editor/FlowEditorRoute';
-import { CreateNodePage } from "@/pages/nodes/create-node/CreateNodePage";
-import { NodeDetailPage } from "@/pages/nodes/node-detail/NodeDetailPage";
-import { EditNodePage } from "@/pages/nodes/edit-node/EditNodePage";
-import { TestNodePage } from "@/pages/nodes/test-node/TestNodePage";
-import { SubnodeDetailPage } from "@/pages/subnodes/subnode-detail/SubnodeDetailPage";
-import { EditSubnodePage } from "@/pages/subnodes/edit-subnode/EditSubnodePage";
-import { EditVersionPage } from "@/pages/subnodes/edit-version/EditVersionPage";
-import { EditVersionPage as EditNodeVersionPage } from "@/pages/nodes/edit-version/EditVersionPage";
-import { CreateSubnodePage } from "@/pages/subnodes/create-subnode/CreateSubnodePage";
-import { ParameterDetailPage } from "@/pages/parameters/parameter-detail/ParameterDetailPage";
-import { CreateParameterPage } from "@/pages/parameters/create-parameter/CreateParameterPage";
-import { EditParameterPage } from "@/pages/parameters/edit-parameter/EditParameterPage";
-import { EdgesPage } from "@/pages/edges/EdgesPage";
-import { DevToolPage } from "@/pages/DevToolPage";
-import { StreamDetailPage } from "@/pages/StreamDetailPage";
-import { FlowsRedirect } from "@/components/redirects/FlowsRedirect";
-import { NodesRedirect } from "@/components/redirects/NodesRedirect";
-import { SubnodesRedirect } from "@/components/redirects/SubnodesRedirect";
-import { ParametersRedirect } from "@/components/redirects/ParametersRedirect";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import { AuthProvider } from "@/contexts/AuthContext";
+import Dashboard from "./pages/Dashboard";
+import OrderManagement from "./pages/OrderManagement";
+import AllOrders from "./pages/AllOrders";
+import WaiterManagement from "./pages/WaiterManagement";
+import WaiterDetail from "./pages/WaiterDetail";
+import ChefManagement from "./pages/ChefManagement";
+import ChefDetail from "./pages/ChefDetail";
+import CustomerManagement from "./pages/CustomerManagement";
+import CustomerDetail from "./pages/CustomerDetail";
+import Advertisements from "./pages/Advertisements";
+import AdvertisementDetail from "./pages/AdvertisementDetail";
+import Reports from "./pages/Reports";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          {/* Login Route - No Layout */}
-          <Route path="/login" element={<LoginPage />} />
-          
-          {/* Main App Routes with Layout */}
-          <Route path="/*" element={
-            <MainLayout>
-              <Routes>
-                <Route path="/" element={<DashboardPage />} />
-                
-                {/* New unified routes */}
-                <Route path="/streams" element={<StreamsPage />} />
-                <Route path="/streams/:streamId" element={<StreamDetailPage />} />
-                <Route path="/alerts" element={<UnifiedAlertsPage />} />
-                <Route path="/reports" element={<UnifiedReportsPage />} />
-                <Route path="/devtool" element={<DevToolPage />} />
-                
-                {/* Legacy routes - keep for backward compatibility */}
-                <Route path="/flows" element={<FlowsRedirect />} />
-                <Route path="/flows/:id" element={<FlowDetailPage />} />
-                <Route path="/flows/:id/edit" element={<FlowEditorRoute />} />
-                <Route path="/nodes" element={<NodesRedirect />} />
-                <Route path="/nodes/new" element={<CreateNodePage />} />
-                <Route path="/nodes/:id" element={<NodeDetailPage />} />
-                <Route path="/nodes/:id/edit" element={<EditNodePage />} />
-                <Route path="/nodes/:id/edit-version" element={<EditNodeVersionPage />} />
-                <Route path="/nodes/:id/test" element={<TestNodePage />} />
-                <Route path="/subnodes" element={<SubnodesRedirect />} />
-                <Route path="/subnodes/:id" element={<SubnodeDetailPage />} />
-                <Route path="/subnodes/create" element={<CreateSubnodePage />} />
-                <Route path="/subnodes/:id/edit" element={<EditSubnodePage />} />
-                <Route path="/subnodes/:id/edit-version" element={<EditVersionPage />} />
-                <Route path="/parameters" element={<ParametersRedirect />} />
-                <Route path="/parameters/new" element={<CreateParameterPage />} />
-                <Route path="/parameters/:id" element={<ParameterDetailPage />} />
-                <Route path="/parameters/:id/edit" element={<EditParameterPage />} />
-                <Route path="/edges" element={<EdgesPage />} />
-                
-                {/* Catch-all route */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </MainLayout>
-          } />
-        </Routes>
-      </BrowserRouter>
+    <ThemeProvider>
+      <AuthProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/orders" element={<OrderManagement />} />
+              <Route path="/all-orders" element={<AllOrders />} />
+              <Route path="/waiters" element={<WaiterManagement />} />
+              <Route path="/waiters/:id" element={<WaiterDetail />} />
+              <Route path="/chefs" element={<ChefManagement />} />
+              <Route path="/chefs/:id" element={<ChefDetail />} />
+              <Route path="/customers" element={<CustomerManagement />} />
+              <Route path="/customers/:id" element={<CustomerDetail />} />
+              <Route path="/advertisements" element={<Advertisements />} />
+              <Route path="/advertisements/:id" element={<AdvertisementDetail />} />
+              <Route path="/reports" element={<Reports />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </AuthProvider>
     </ThemeProvider>
   </QueryClientProvider>
 );
